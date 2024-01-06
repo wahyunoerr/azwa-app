87ca30320edd7b13dd64e0d90582db30b45bf486
tengkuzainul <tengkuzainula04@gmail.com>
2024-01-06 23:02:19 +0700

    gas bang ini udah aman tamplate nya

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -42,57 +42,106 @@
                         </form>
                     </div>
                 </div>
+                <div class="col-md-8">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">List Tables Supplier</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th width="20px">No</th>
+                                        <th>Supplier Name</th>
+                                        <th>Product Name</th>
+                                        <th>Qty Product In</th>
+                                        <th class="text-center" width="20px">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($data as $sup)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $sup->nama }}</td>
+                                            <td>{{ $sup->prd_name }}</td>
+                                            <td>{{ $sup->prd_masuk }}</td>
+                                            <td>
+                                                <a href="{{ route('supplier.edit', $sup->id) }}"><i
+                                                        class="fas fa-solid fa-pen"></i></a> |
+                                                <a href="{{ route('supplier.delete', $sup->id) }}" class="text-danger"><i
+                                                        class="fas fa-solid fa-trash"></i></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th width="20px">No</th>
+                                        <th>Supplier Name</th>
+                                        <th>Product Name</th>
+                                        <th>Qty Product In</th>
+                                        <th class="text-center" width="20px">Action</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
             @endhasrole
             {{-- End --}}
 
             {{-- Data Tables --}}
-            <div class="col-md-8">
-                <div class="card card-primary">
-                    <div class="card-header">
-                        <h3 class="card-title">List Tables Supplier</h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th width="20px">No</th>
-                                    <th>Supplier Name</th>
-                                    <th>Product Name</th>
-                                    <th>Qty Product In</th>
-                                    <th class="text-center" width="20px">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($data as $sup)
+            @hasrole('admin')
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">List Tables Supplier</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
                                     <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $sup->nama }}</td>
-                                        <td>{{ $sup->prd_name }}</td>
-                                        <td>{{ $sup->prd_masuk }}</td>
-                                        <td>
-                                            <a href="{{ route('supplier.edit', $sup->id) }}"><i
-                                                    class="fas fa-solid fa-pen"></i></a> |
-                                            <a href="{{ route('supplier.delete', $sup->id) }}" class="text-danger"><i
-                                                    class="fas fa-solid fa-trash"></i></a>
-                                        </td>
+                                        <th width="20px">No</th>
+                                        <th>Supplier Name</th>
+                                        <th>Product Name</th>
+                                        <th>Qty Product In</th>
+                                        <th class="text-center" width="20px">Action</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <th width="20px">No</th>
-                                    <th>Supplier Name</th>
-                                    <th>Product Name</th>
-                                    <th>Qty Product In</th>
-                                    <th class="text-center" width="20px">Action</th>
-                                </tr>
-                            </tfoot>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($data as $sup)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $sup->nama }}</td>
+                                            <td>{{ $sup->prd_name }}</td>
+                                            <td>{{ $sup->prd_masuk }}</td>
+                                            <td>
+                                                <a href="{{ route('supplier.edit', $sup->id) }}"><i
+                                                        class="fas fa-solid fa-pen"></i></a> |
+                                                <a href="{{ route('supplier.delete', $sup->id) }}" class="text-danger"><i
+                                                        class="fas fa-solid fa-trash"></i></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th width="20px">No</th>
+                                        <th>Supplier Name</th>
+                                        <th>Product Name</th>
+                                        <th>Qty Product In</th>
+                                        <th class="text-center" width="20px">Action</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
                     </div>
-                    <!-- /.card-body -->
                 </div>
-            </div>
+            @endhasrole
             {{-- End --}}
             <!-- /.row -->
         </div>
